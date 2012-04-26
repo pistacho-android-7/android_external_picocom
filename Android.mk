a8c1ed9d30b8bb7ac7803dbcc9726be7034ae4c2
Sergey Shcherbakov <shchers@gmail.com>
2012-04-27 00:09:44 +0300

    Initial commit
    This is port of picocom 1.7 to Android OS. On this step
    was created Android.mk.
    
    Signed-off-by: Sergey Shcherbakov <shchers@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	picocom.c \
+	term.c
+
+LOCAL_SHARED_LIBRARIES := \
+    libc libcutils libutils
+
+LOCAL_MODULE_TAGS := optional
+
+# Build shared library
+LOCAL_SHARED_LIBRARIES += \
+    libcutils libutils
+LOCAL_LDLIBS += -lpthread
+LOCAL_LDLIBS += -lrt
+LOCAL_CFLAGS += -DRIL_SHLIB
+LOCAL_CFLAGS += -DVERSION_STR="\"1.7.1\""
+# Enabling baudrate more then 115200kpbs
+LOCAL_CFLAGS += -DHIGH_BAUD
+LOCAL_CFLAGS += -Wall
+
+LOCAL_MODULE:= picocom
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+include $(BUILD_EXECUTABLE)
